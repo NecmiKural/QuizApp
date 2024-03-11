d298d879613a10fa2aa9a2f927843f7290d64fcf
neo <necmibkural@gmail.com>
2024-03-11 11:38:06 +0300

    auth disabled for testing before implementing security auth

--- spring/src/main/java/com/example/quizapp/QuizAppApplication.java
+++ spring/src/main/java/com/example/quizapp/QuizAppApplication.java
@@ -2,10 +2,10 @@ package com.example.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 // for testing before implementing security auth, disables basic auth and we can able to test all requests
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+// or security.basic.enabled=false and management.security.enabled=false on application-dev.properties
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class QuizAppApplication {
 
     public static void main(String[] args) {
