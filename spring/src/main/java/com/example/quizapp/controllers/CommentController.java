d4e8e028117394734e819da48a05aa3723ac4441
neo <necmibkural@gmail.com>
2024-03-11 08:54:34 +0300

    - comment service and controller created - consistency improved and fixed

--- spring/src/main/java/com/example/quizapp/controllers/CommentController.java
+++ spring/src/main/java/com/example/quizapp/controllers/CommentController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @RequestMapping("/comments")
 public class CommentController {
 
-    private CommentService commentService;
+    private final CommentService commentService;
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
