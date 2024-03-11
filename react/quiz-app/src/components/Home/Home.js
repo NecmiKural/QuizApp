90744f9d656ff3f165d9b17242e59680fd0b68d2
neo <necmibkural@gmail.com>
2024-03-11 15:51:11 +0300

    style added and reorder components

--- react/quiz-app/src/components/Home/Home.js
+++ react/quiz-app/src/components/Home/Home.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Post from "../Post/Post";
+import './Home.scss';
 
 function Home() {
 
-    return (
-        <div>
-            Home
-            <Post/>
-        </div>
-    );
+    // the states we need. we will initialize here and then use in forward
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        fetch("/posts")
+            .then(r => r.json())
+            .then((result) => {
+                setIsLoaded(true);
+                setPostList(result);
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            })
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className='container'>
+                Home
+                {
+                    postList.map(post => (
+                        <Post title={post.title} text={post.text} />
+
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default Home;
--- react/quiz-app/src/components/Post/Post.js
+++ react/quiz-app/src/components/Post/Post.js
@@ -1,39 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import './Post.scss';
 
+function Post(props) {
+    const { title, text } = props;
 
-function Post() {
-    // the states we need. we will initialize here and then use in forward
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [postList, setPostList] = useState([]);
-
-    useEffect(() => {
-        fetch("/posts")
-            .then(r => r.json())
-            .then((result) => {
-                setIsLoaded(true);
-                setPostList(result);
-            }, (error) => {
-                setIsLoaded(true);
-                setError(error);
-            })
-    }, [])
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
-        return (
-            <ul>
-                {postList.map(post => (
-                    <li key={post.id}>
-                        {post.title} {post.text}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+    return (
+        <div className='postContainer'>
+            <h1>{title}</h1>
+            <p>{text}</p>
+        </div>
+    );
 }
 
 export default Post;
