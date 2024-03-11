cc9c8d14f10018e73a3f2f655baa92c55f3691a6
neo <necmibkural@gmail.com>
2024-03-11 10:51:06 +0300

    -react connected and tested for post

--- react/quiz-app/src/index.js
+++ react/quiz-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Post from "./components/Post/Post";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Post/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
