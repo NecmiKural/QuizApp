190ed4664069baa9b0d60db50c5bcbfe574da8a1
neo <necmibkural@gmail.com>
2024-03-18 19:29:33 +0300

    should update

--- spring/src/main/java/com/example/quizapp/security/JwtTokenProvider.java
+++ spring/src/main/java/com/example/quizapp/security/JwtTokenProvider.java
@@ -7,8 +7,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class JwtTokenProvider {
@@ -34,5 +40,26 @@ public class JwtTokenProvider {
         return Long.parseLong(claims.getSubject());
     }
 
-    //20:13
+    boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(token);
+            return !isTokenExpired(token);
+        } catch (SignatureException e) {
+            return false;
+        } catch (MalformedJwtException e) {
+            return false;
+        } catch (ExpiredJwtException e) {
+            return false;
+        } catch (UnsupportedJwtException e) {
+            return false;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+
 }
